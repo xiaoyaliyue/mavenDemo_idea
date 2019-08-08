00d4e0e5954633d343d5c682c64d73621a2c67f4
xiaoyaliyue <826365976@qq.com>
2019-08-08 15:30:53 +0800

    Update updateLoadScript.sh
    Windows上传包到Linux服务器

--- updateLoadScript.sh
+++ updateLoadScript.sh
@@ -1,4 +1,5 @@
 --连接服务器，上传文件
-open sftp://work:qazwsx@123@47.112.139.67:10022
-put C:\Users\tiger\.jenkins\workspace\pipelineDemo_mvn\target\mavenDemo_idea-1.6.war
-exit
+echo "开始上传文件"
+pscp -l work -pw qazwsx@123 -r C:\Users\tiger\.jenkins\workspace\pipelineDemo_deploy\target\mavenDemo_idea-1.6.war work@47.112.139.67:/home/work/apache-tomcat-7.0.79/webapps
+echo "上传文件成功"
+

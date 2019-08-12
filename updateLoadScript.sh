4740fa78ef9a3f370c1a450584579963428ce227
xiaoyaliyue <826365976@qq.com>
2019-08-12 14:34:54 +0800

    Update updateLoadScript.sh

--- updateLoadScript.sh
+++ updateLoadScript.sh
@@ -1,6 +1,7 @@
 ##--连接服务器，上传文件
 echo "开始上传文件"
-pscp -l work -pw qazwsx@123 -r C:\Users\tiger\.jenkins\workspace\pipelineDemo_deploy\target\mavenDemo_idea-1.6.war work@47.112.139.67:/home/work/deploy
+#pscp -l work -pw qazwsx@123 -r C:\Users\tiger\.jenkins\workspace\pipelineDemo_deploy\target\mavenDemo_idea-1.6.war work@47.112.139.67:/home/work/deploy
+scp -l work -pw qazwsx@123 -r C:\Users\tiger\.jenkins\workspace\pipelineDemo_deploy\target\mavenDemo_idea-1.6.war work@47.112.139.67:/home/work/deploy
 echo "上传文件成功"
 
 
